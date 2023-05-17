467ba8167e1d5615a3abb48fdc968305ac571f16
ppedrobg <rm551061@fiap.com.br>
2023-05-17 14:28:36 -0300

    login

---
+++ js/login.js
@@ -0,0 +1,79 @@
+//LISTA DE USUÁRIOS
+let listaDeUsuarios = [
+
+    {
+        nomeCompleto : "José das Couves",
+        nomeUsuario : "jose",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "João Paulino",
+        nomeUsuario : "joao",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "Maria Tomaite",
+        nomeUsuario : "maria",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "Paulo da Selva",
+        nomeUsuario : "paulo",
+        senhaUsuario : "123456"
+    }
+];
+
+localStorage.setItem("listaUser" ,JSON.stringify(listaDeUsuarios));
+
+
+
+addEventListener("click", (evento)=>{
+     
+    let userInput = document.querySelector("#idUser"); 
+    let passInput = document.querySelector("#idPass"); 
+        
+        //MOSTRA SENHA NO OLHINHO
+        if(evento.target.className == "fa fa-eye" || evento.target.className == "fa fa-eye-slash"){
+
+            evento.target.setAttribute("style","cursor:pointer")
+
+            if(passInput.getAttribute("type") == "password"){
+                passInput.setAttribute("type","text");
+                evento.target.setAttribute("class","fa fa-eye-slash")  
+            }else{
+                evento.target.setAttribute("style","cursor:pointer");
+                evento.target.setAttribute("class","fa fa-eye");  
+                passInput.setAttribute("type","password"); 
+            }
+
+        }
+
+        let listaDeUsuariosRecuperada = JSON.parse(localStorage.getItem("listaUser"));
+
+        if(evento.target.id == "btnSubmit"){
+        
+        try{
+                listaDeUsuariosRecuperada.forEach((usuario)=>{
+
+                    if(userInput.value == usuario.nomeUsuario && passInput.value == usuario.senhaUsuario){
+                        throw "USUÁRIO VALIDADO!";
+                    }
+                });
+
+                throw "USUÁRIO OU SENHA INCORRETOS!";
+
+    }catch(msg){
+        if(msg == "USUÁRIO VALIDADO!"){
+            //Redirect
+            window.location.href = "../sucesso.html";
+
+        }else{
+            console.log("USUÁRIO OU SENHA INCORRETOS!");
+        }
+    }
+
+  }
+});
--- js/script.js
+++ js/script.js
@@ -1,182 +1,117 @@
-/*
-    O HOISTING ou elevação, coloca todas as VAR no topo do programa. Isso faz com que se existirem variáveis com o mesmo nome, eles se sobrepoem, independente do lugar no código.
-    VAR é um valor que pode ser alterado, mas pode ser alterado em qualquer parte do código, por causa do HOISTING.
-    
-    LET ele é um valor variável, mas ele só pode ser alterado no mesmo bloco, sendo diferente do VAR
-    
-    CONST é um valor que não pode ser alterado quando está sendo executado, PRECISA SER INICIALIZADO
-*/
-
 // var nome = "Luizinho";
-// let nome2 = "Vitinho";
-// const nome3 = "Miguelito";
-
-// console.log(nome);
-// console.log(nome2);
-// console.log(nome3);
 
 // if(nome != ""){
-//     let nome = "Nicolly"
+//     let nome = "Nicolly";
 // }
 
-// console.log("Qual nome vai ser impresso aqui!?", nome);
-
-
-// let n1 = 10;
-// let n2 = 5;
-
-// // IF TERNÁRIO - descobrir se o número é par ou impar
-// // ele verifica 0 e 1, onde o 0 é negativo, condição falsa, aplicando o else (impar)
-// let resultado = !(n1 % n2) ? "PAR" : "IMPAR"
-
+// console.log(nome);
+//Quando declaramos uma variável com var
+// o Javascript aplica um processo nestas que chamamos de HOISTING.
+//O HOISTING ou elevação, coloca todas as variáveis do tipo var no topo do programa.
+//Isso faz com que se existirem variáveis com mesmo nome, elas se sobrepõem.
+// let nr1 = 10;
+// let nr2 = 5;
+// //IF TERNÁRIO - DESCOBRINDO SE O NÚMERO É PAR OU IMPAR
+// let resultado = !(nr1 % nr2) ? "PAR" : "IMPAR";
 // console.log(resultado);
 
+//Recuperar um elemento do HTML com a função getElementById(parâmetro)
+// const mn = document.getElementById("menu");
+// console.log(mn.textContent);
 
-// Recuperar um elemento do HTML com a função getElementById(parâmetro);
-    // const mn = document.getElementById("menu");
-    // console.log(mn.textContent);
+// //Recuperando uma collection de elementos...
+// const aElements = document.getElementsByTagName("a");
+// const aElementsArray = [...aElements];
 
+// aElementsArray.forEach((a)=>{
+//     console.log(a);
+// });
 
+// //Exercícios
+// //1 - Recupere uma coleção de imagens da página e imprima o
+// // atributo src...
+// const imgElements = [...document.getElementsByTagName("img")];
+// // const imgElementsArray = [...imgElements];
+// // console.log(imgElements);
 
+// //IMPRIMINDO O ATRIBUTO SRC do elemento com o forEach
+// imgElements.forEach((img)=>{
+//     console.log(`Path das imagens : ${img.src}`);
+// });
 
+// const botao = document.querySelector("div > button");
 
-//                 AULA 18 - 02/05    AULA 18 - 02/05    AULA 18 - 02/05
-//                 AULA 18 - 02/05    AULA 18 - 02/05    AULA 18 - 02/05
-
-//Declarando uma array em JavaScript
-    // let n1 = [1, 2, 3, 4, 5];
-    // let n2 = [6, 7, 8, 9, 10];
-    // let n3 = [n1, n2];
-    // console.log(n1);
-    // console.log(n2);
-
-
-// operador SPREAD ...
-    // let n3 = [...n1,...n2];
-
-    // n3.forEach( (numero)=>{
-    //     console.log("Item do Array : " + numero);
-    // } );
-
-
-// Recuperando uma colection de elementos...
-    // const aElements = document.getElementsByTagName("a");
-    // const aElementsArray = [...aElements];
-    // aElementsArray.forEach((a)=>{
-    //     console.log(a);
-    // });
-
-
-//Recupere uma coleção de imagens da página e imprima o atributo SRC...
-    // const imgElements = [...document.getElementsByTagName("img")];
-    // imgElements.forEach((img)=>{
-    //     console.log(`Path das imagens: ${img.src}`);
-    // });
-
-
-//Exibir um alert quando ele detectar um evento de click no botão
-    // const botao = document.querySelector("div > button");
-    // console.log(botao);
-    // botao.addEventListener("click", ()=>{
-    //     alert("GoodBye My Friend!!!");
-    // });
-
-/* ANOTAÇÕES DA AULA 18 - 02-05
-    
-    "()" variavel que receberá o conteúdo da iteração (uma volta do loop)
-    
-    "<>"" - esteriótipo de uma coleção de array, vem antes de ()
-    
-    "SPREAD" - pega um array e espalha o conteudo
-    
-    "forEach" - loop para exibir cada elemento dentro de um outro elemento/variável (tipo python com o For)
-    querySelector - Pega o elemento com base no texto do css
-*/
+// botao.addEventListener("click", ()=>{
+//     alert("TCHAU!!!");
+// });
 
 
 
+// //Declarando um array em Javascript
+// let nr1 = [1,2,3,4,5];
+// let nr2 = [6,7,8,9,10];
+// // let nr3 = [nr1,nr2];
+// console.log(nr1);
+// console.log(nr2);
 
+// //OPERADOR SPREAD ...
+// let nr3 = [...nr1,...nr2];
+// console.log("NOVO ARRAY " + nr3);
 
+// nr3.forEach( (numero)=>{
+//     console.log("ITEM DO ARRAY : " + numero);
+// } );
 
+// let frutas = ["laranja","banana","acerola","abacate","caqui","uva"];
+// console.log(frutas);
+// //Inserir itens ao final do array com o método push(iten);
+// frutas.push("kiwi");
+// console.log(frutas);
+// //Inserir itens no início do array com o unshift();
+// frutas.unshift("melancia");
+// console.log(frutas);
+// //Remover itens do final do array com o método pop();
+// frutas.pop();
+// console.log(frutas);
+// //Remover itens do inicio do array com o método shift();
+// frutas.shift();
+// console.log(frutas);
 
-// AULA 19 - [04-05]   AULA 19 - [04-05]   AULA 19 - [04-05]   AULA 19 - [04-05]
-// AULA 19 - [04-05]   AULA 19 - [04-05]   AULA 19 - [04-05]   AULA 19 - [04-05]
+// //Realizando a busca de um item no array usando o método indexOf(iten)
+// //Quando usamos o indexOf(iten) ele retorna o indice do item procurado.
+// // let indice = frutas.indexOf("acerola");
+// // console.log(frutas[indice]);
 
+// //Utilizando o método splice(indice do item, qtd de vezes que ele será removido), para remover um iten do array em conjunto com o indexOf(iten).
+// let indice = frutas.indexOf("abacate");
+// console.log("FRUTA QUE ESTAVA NO INDICE ANTES DA REMOÇÃO : " + frutas[indice]);
+// frutas.splice(indice,1);
+// console.log("FRUTA QUE FICOU NO INDICE DEPOIS DA REMOÇÃO : " + frutas[indice]);
+// console.table(frutas);
 
-//MANIPULAÇÃO DOS DADOS DO ARRAY:
+//Estrutura de repetição para iteração de collections
+let frutas = ["laranja","banana","acerola","abacate","caqui","uva"];
+const aElements  = document.querySelectorAll("a");
+//Loop for
+// for(let x = 0; x < aElements.length ; x++){
+//     console.log("Valor de x = " + aElements[x].textContent);
+// }
+// for (const fruta of frutas) {
+//     console.log("Frutas " + fruta);
+// }
 
-let frutas = ["Manga", "Banana", "Laranja", "Maçã", "Melancia", "Framboesa"];
+// for (const indice in frutas) {
+//     console.log("Fruta " + frutas[indice]);
+// }
 
-        /* INSERÇÃO DE DADOS NO DINAL DO ARRAY [".push"] */
-    // frutas.push("kiwi");
-    // console.log(frutas);
-
-
-        /* INSERÇÃO DE DADOS NO INICIO DO ARRAY [".unshift"] */
-    // frutas.unshift("Melão");
-    // console.log(frutas);
-
-
-        /* REMOVER ITEM NO FINAL DO ARRAY [".pop"] */
-    // frutas.pop();
-    // console.log(frutas);
-
-
-        /* REMOVER ITEM NO INICIO DO ARRAY [".shift"] */
-    // frutas.shift();
-    // console.log(frutas);
-
-
-        /* BUSCA DE UM ITEM NO ARRAY [".indexOf(item)"]
-           indexOf retorna o indice do item, ou seja, a posição dele no array */
-    // let indice = frutas.indexOf("Banana");
-    // console.log(indice);
-
-
-        /* REMOVER UM ITEM DO ARRAY 
-           EM CONJUNTO COM O indexOf 
-           [".splice(<indice>, <qtd vzs será removido>)"] */
-    // console.log("Frutas ANTES de remover LARANJA = " + frutas)
-
-    // let indice = frutas.indexOf("Laranja");
-    // console.log("Laranja está na posição: " + indice);
-
-    // frutas.splice(indice, 1);
-    // console.table("Frutas DEPOIS de remover LARANJA = " + frutas);
-
-
-
-        /* ESTRUTURA DE REPETIÇÃO PARA ITERAÇÃO DE COLLECTIONS */
-    // const aElements = document.querySelectorAll("a");
-    // for(let x = 0; x < aElements.length; x++){
-    //     console.log("Valor = " + aElements[x].textContent);
-    // }
-
-
-        /* forOf 
-        Ele gera um item do array e você pode exibir ele */
-    // for (const fruta of frutas) {
-    //     console.log("Frutas: " + fruta);
-    // }
-
-    
-        /* forIn 
-        Ele pega o lugar do item dentro do array, mas colocando [indice], ele pega o indice no array e puxa o nome do objeto */
-    // for (const indice in frutas) {
-    //     console.log("Fruta: " + frutas[indice]);
-    // }
-    
-    
-        /* MAP 
-        Ele necessita de uma ArrowFunction, atribuindo o valor na variavel, MAIS COMPLETO*/
-    // frutas.map( (fruta, key)=>{
-    //     console.log(`Fruta nº${key+1} = ${fruta}`);
-    // } );
-
+//MAP
+// frutas.map( (fruta,key)=>{
+//     console.log(`Fruta nr:${key+1} ${fruta}`);
+// });
 
 
 const inputUser = document.querySelector("#idUser");
+
 inputUser.addEventListener("focus", ()=>{
-    console.log(inputUser);
-    inputUser.setAttribute("style", "outline-color: #ff0000;")
+    inputUser.setAttribute("style","outline-color:#ff0000;");
 });
